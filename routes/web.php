f15c5dc8b8e26bd7fc7e81f701a9c71ca2d22d02
arkaadino <arkaaldino28@gmail.com>
2025-01-15 08:43:30 +0700

    halaman login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 
 
-Route::get('/', function () {
-    return view('home.dashboard');
+
+Route::get('/login', [Logincontroller::class,'showLogin'])->name('login');
+Route::post('/actionLogin', [Logincontroller::class,'actionLogin'])->name('actionLogin');
+Route::get('/logout', [Logincontroller::class,'Logout'])->name('Logout');
+
+Route::middleware(['auth'])->group(function(){
+
+Route::get( '/', [DashboardController::class,'index']);
 
    
-});
-
 Route::get('/user', [UserController::class,'index']);
 Route::get('/user/tambah', [UserController::class,'create']);
 Route::post('/user/simpan', [UserController::class,'store']);
@@ -18,3 +24,4 @@ Route::post('/user/simpan', [UserController::class,'store']);
 Route::get('/user/{id}/edit', [UserController::class,'edit']);
 Route::post('/user/{id}/update', [UserController::class,'update']);
 Route::get('/user/{id}/delete', [UserController::class,'destroy']);
+});

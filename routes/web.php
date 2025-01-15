bd3579becd29c2aebe7c1ab13f3d225b8615a33d
ikhsanrestu <114740303+ikhsanrestu@users.noreply.github.com>
2025-01-15 09:28:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,24 @@ use Illuminate\Http\Request;
 
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 
 
-Route::get('/', function () {
-    return view('home.dashboard');
-
-    Route::get('/user', [UserController::class,'index']);
+    
     Route::get('/login', [Logincontroller::class,'showLogin'])->name('login');
-Route::post('/actionLogin', [Logincontroller::class,'actionLogin'])->name('actionLogin');
-Route::get('/logout', [Logincontroller::class,'Logout'])->name('Logout');
+    Route::post('/actionLogin', [Logincontroller::class,'actionLogin'])->name('actionLogin');
+    Route::get('/logout', [Logincontroller::class,'Logout'])->name('Logout');
 
-Route::middleware(['auth'])->group(function(){
+    Route::middleware(['auth'])->group(function(){
 
-Route::get( '/', [DashboardController::class,'index']);
-
-
-Route::get('/user/tambah', [UserController::class,'create']);
-Route::post('/user/simpan', [UserController::class,'store']);
-
-Route::get('/user/{id}/edit', [UserController::class,'edit']);
-Route::post('/user/{id}/update', [UserController::class,'update']);
-Route::get('/user/{id}/delete', [UserController::class,'destroy']);
+    Route::get( '/', [DashboardController::class,'index']);
+    Route::get('/user', [UserController::class,'index']);
+    Route::get('/user/tambah', [UserController::class,'create']);
+    Route::post('/user/simpan', [UserController::class,'store']);
+    Route::get('/user/{id}/edit', [UserController::class,'edit']);
+    Route::post('/user/{id}/update', [UserController::class,'update']);
+    Route::get('/user/{id}/delete', [UserController::class,'destroy']);
 
 
 
